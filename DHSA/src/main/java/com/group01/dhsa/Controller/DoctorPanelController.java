a2d4703c9a84f253549ef864f08ced1bffa1b989
mapic <mapicella420@gmail.com>
2025-01-19 17:55:04 +0100

    Fix Prograss upload

--- DHSA/src/main/java/com/group01/dhsa/Controller/DoctorPanelController.java
+++ DHSA/src/main/java/com/group01/dhsa/Controller/DoctorPanelController.java
@@ -2,6 +2,7 @@ package com.group01.dhsa.Controller;
 
 import com.group01.dhsa.EventManager;
 import com.group01.dhsa.ObserverPattern.EventObservable;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
@@ -100,5 +101,8 @@ public class DoctorPanelController {
 
         Stage stage = new Stage();
         return fileChooser.showOpenDialog(stage);
+    }
+
+    public void onViewCsvMenuClick(ActionEvent actionEvent) {
     }
 }
