45955ba1908614dd879b452cb48ba6d640da6d31
mapic <mapicella420@gmail.com>
2025-01-22 13:45:24 +0100

    Fix deceased patient

--- DHSA/src/main/java/com/group01/dhsa/Controller/DischargePanelController.java
+++ DHSA/src/main/java/com/group01/dhsa/Controller/DischargePanelController.java
@@ -216,7 +216,7 @@ public class DischargePanelController {
             for (Bundle.BundleEntryComponent entry : response.getEntry()) {
                 Patient patient = (Patient) entry.getResource();
 
-                if (!patient.hasDeceasedBooleanType()) {
+                if (patient.hasDeceasedBooleanType()) {
                     MenuItem item = new MenuItem(patient.getIdentifier().getFirst().getValue());
                     item.setId("item" + i);
                     i++;
